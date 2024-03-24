b86eb7dd1c9c9ddf8933666975ccf4abdbca4567
Mark Nyanjui <mark.warunge@student.moringaschool.com>
2024-03-24 17:44:40 +0300

    Complete.lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var multiply;
+let num1 = 31
+let num2 = 2
+var multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1
+let num3 = 18
+let num4 = 7
+let mod = num3 % num4
+
+const max = Math.max(5,14,19,20,12);
